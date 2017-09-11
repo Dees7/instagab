18fdb025877202f287053ead53a4e786457bbf3a
Denis Taranushin <tda@oxip.me>
2017-09-11 16:17:05 +0500

    first commit

---
+++ instagram-image-dumper.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+lastid=""
+HTTPS_PROXY=http://127.0.0.1:8123
+https_proxy=http://127.0.0.1:8123
+HTTP_PROXY=http://127.0.0.1:8123
+http_proxy=http://127.0.0.1:8123
+
+function geturls()
+{
+urls=( $(echo ${json} | jq -r ".items[].images.standard_resolution.url, .items[].videos.standard_resolution.url" ) )
+}
+
+function getjson()
+{
+json=$(wget --quiet -O - "http://instagram.com/${1}/media?max_id=${2}")
+}
+
+function downloadmedia()
+{
+for url in "${urls[@]}"
+do
+    if [[ "${url}" != "null" ]]
+    then
+        echo ${user}  ${url}
+        wget --quiet -c ${url} &
+    fi
+done
+}
+
+function downloadall()
+{
+getjson ${1} ${lastid}
+geturls
+downloadmedia
+ifmore=$(echo ${json} | jq -r ".more_available")
+if [[ -f debug ]]; then echo  "${ifmore}"; fi
+if [[ -f skip ]]; then ifmore="false"; fi
+if [[ "${ifmore}" == "true" ]]
+then
+    lastid=$(echo ${json} | jq -r ".items[19].id" | sed -e 's/\"//g')
+    if [[ -f debug ]]; then echo "${lastid}"; fi
+    downloadall ${1}
+fi
+}
+
+while read user
+do
+    lastid=""
+    if [[ -d "${user}" ]]
+    then
+       echo "${user} exists, crawling."
+    else
+       mkdir "${user}"
+    fi
+    if cd "${user}"
+    then
+        downloadall ${user}
+        cd ..
+    else
+        echo "Could not enter directory"
+    fi
+done < users.txt
